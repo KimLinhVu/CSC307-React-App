6d5769ef14e9fb3a9c3fae7a9e045c753de42e27
KimLinhVu <klvbubble@gmail.com>
2023-09-25 18:57:48 -0700

    added axios to link frontend with backend

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,8 +1,10 @@
-import express from "express";
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
-const port = 8001;
+const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 const users = { 
--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,9 +1,18 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Table from "./Table";
 import Form from "./Form";
+import axios from 'axios';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetchAll().then(result => {
+      if (result) {
+        setCharacters(result);
+      }
+    });
+  }, []);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => { 
@@ -24,5 +33,16 @@ function MyApp() {
       <Form handleSubmit={updateList} />
     </div>
   );
+}
+
+async function fetchAll() {
+  try {
+    const response = await axios.get('http://localhost:8000/users');
+    return response.data.users_list;
+  }
+  catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 export default MyApp;
